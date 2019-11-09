5b2f85ab1c9998e3640401a8700051ca205a0bab
W0nderWoman <guptamayuri.acmn@gmail.com>
2019-11-09 21:01:33 +0530

    MainUI

---
+++ app/src/main/java/com/example/gupta/copier/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gupta.copier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//TODO----Fab drawer
+//TODO----pdf support
+//TODO----share support
